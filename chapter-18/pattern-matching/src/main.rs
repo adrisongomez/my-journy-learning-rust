9ec9453db086228697a3c768f52489ac7a857893
Adrison Gomez <agomez@resonance.nyc>
2023-05-30 21:55:49 -0400

    Chapter 18: Pattern matching

---
+++ chapter-18/pattern-matching/src/main.rs
@@ -0,0 +1,67 @@
+fn main() {
+    let x: Option<i32> = Some(5);
+
+    match x {
+        None => None,
+        Some(y) => Some(y + 1),
+    };
+
+    println!("Hello, world!");
+
+    let mut stack = vec![1, 2, 3];
+
+    while let Some(x) = stack.pop() {
+        println!("{}", x);
+    }
+    let p = Point { x: 0, y: 7 };
+
+    let Point { x: a, y: b } = p;
+    assert_eq!(0, a);
+    assert_eq!(7, b);
+    let p = Point { x: 0, y: 7 };
+
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {x}"),
+        Point { x: 0, y } => println!("On the y axis at {y}"),
+        Point { x, y } => {
+            println!("On neither axis: ({x}, {y})");
+        }
+    }
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, .., last) => {
+            println!("Some numbers: {first}, {last}");
+        }
+    }
+
+    let num = Some(4);
+
+    match num {
+        Some(x) if x % 2 == 0 => println!("The number {} is even", x),
+        Some(x) => println!("The number {} is odd", x),
+        None => (),
+    }
+
+    enum Message {
+        Hello { id: i32 },
+    }
+
+    let msg = Message::Hello { id: 5 };
+
+    match msg {
+        Message::Hello {
+            id: id_variable @ 3..=7,
+        } => println!("Found an id in range: {}", id_variable),
+        Message::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        Message::Hello { id } => println!("Found some other id: {}", id),
+    }
+}
+
+struct Point {
+    x: i32,
+    y: i32,
+}
